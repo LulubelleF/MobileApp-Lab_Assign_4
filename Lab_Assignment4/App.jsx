87af5c344bbc448de8345bb31b761ae7820008f0
LulubelleF <Lulubelle.Fontelo@edu.sait.ca>
2025-03-22 12:29:37 -0600

    component initilize

---
+++ Lab_Assignment4/App.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import { View, FlatList, Text, StyleSheet } from "react-native";
+import ToDoForm from "./component/ToDoForm";
+
+export default function App() {
+  return (
+    
+  );
+}
+
+const styles = StyleSheet.create({
+  },
+});
